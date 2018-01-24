f23e2cc8f3530df2564f21d90966cba1329b748d
Davide <davide.iaiunese@gmail.com>
2018-01-24 21:17:58 +0100

    Deleted TODO.md and modified keep-sum.js

--- keep-sum.js
+++ keep-sum.js
@@ -22,4 +22,3 @@ console.log(reduce([1,2,3],3)); // [3,3]
 console.log(reduce([8,12,16,20,4],20)); // [20,20,20] // sum inserted in place of the number of the first index
 console.log(reduce([14,8,6,8,3,5],16)); // [14,16,6,3,5]
 console.log(reduce([1,6,9,8],25));  // return the array [1,6,9,8] as no sum
-
